ef7e84f94a511f215c1fc9b3884506982a330a6c
jiha <zhsclq5324@naver.com>
2024-10-07 13:47:20 +0900

    다익스트라 간소화

---
+++ 다익스트라알고리즘/priorityQueue.js
@@ -0,0 +1,22 @@
+class PriorityQueue {
+  constructor() {
+    this.values = [];
+  }
+  enqueue(val, priority) {
+    this.values.push({ val, priority });
+    this.sort(); // add 하고나면 다시 정렬
+  }
+  // 가장 작은 priority를 가진 값이 나와야 함
+  dequeue() {
+    return this.values.shift();
+  }
+  sort() {
+    this.values.sort((a, b) => a.priority - b.priority);
+  }
+}
+
+const pq = new PriorityQueue();
+
+pq.enqueue("B", 3);
+pq.enqueue("C", 5);
+pq.enqueue("D", 2);
